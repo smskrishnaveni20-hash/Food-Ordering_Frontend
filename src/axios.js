704ce80af25d6bd3f79971fe7f99b8ce052d5560
smskrishnaveni20-hash <smskrishnaveni20@gmail.com>
2026-06-27 21:37:26 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,17 +1,39 @@
+// import axios from "axios";
+
+// const isDevelopment = import.meta.env.MODE === "development";
+
+// const BASE_URL = isDevelopment? 
+// import.meta.env.VITE_API_BASE_URL_LOCAL: import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
+
+// const API = axios.create({
+//   baseURL: "BASE_URL",
+// });
+
+// API.interceptors.request.use(config => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const isDevelopment = import.meta.env.MODE === "development";
 
-const BASE_URL = isDevelopment? 
-import.meta.env.VITE_API_BASE_URL_LOCAL: import.meta.env.VITE_API_BASE_URL_DEPLOY;
-
+const BASE_URL = isDevelopment
+  ? import.meta.env.VITE_API_BASE_URL_LOCAL
+  : import.meta.env.VITE_API_BASE_URL_DEPLOY;
 
 const API = axios.create({
-  baseURL: "BASE_URL",
+  baseURL: BASE_URL,  
 });
 
 API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("access");  
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
